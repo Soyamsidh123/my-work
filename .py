746109da61a06f771ad9ab8d464af10f5a2a32ba
Soyamsidh123 <119417892+Soyamsidh123@users.noreply.github.com>
2024-08-04 01:09:47 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 00:06:20 2024
+
+@author: LENOVO
+"""
+
+n = int(input())
+for i in range(1,n+1):
+    print(i,end="")
---
+++ if else.py
@@ -0,0 +1,5 @@
+n = int(input("enter a number : "))
+if(n%2!=0 or 6<=n<=20):
+    print("weird")
+elif(2<=n<=5 or n>20):
+    print("not weird")
